418e65b138070e55ba7f8ebca45802a927e56edf
csl799 <1794307042@qq.com>
2026-05-08 16:18:52 +0800

    agent主体

--- test_react_agent/agent.py
+++ test_react_agent/agent.py
@@ -1,5 +1,25 @@
+import click
+import os
+
 from Use_tools import run_terminal_command,read_file,write_to_file
+from ReActAgent import ReActAgent
+
+@click.command()
+@click.argument("project_directory",
+                type = click.Path(exists = True,file_okay = False,dir_okay = True))
+def main(project_directory):
+    project_dic = os.path.abspath(project_directory)
+
+    tools = [read_file,write_to_file,run_terminal_command]
+    agent = ReActAgent(tools=tools,model="deepseek-chat",project_directory=project_dic)
+
+    task = input("Task: ")
+
+    final_answer = agent.run(task)
+
+    print(f"\n\n Final Answer: {final_answer}")
 
 
-
+if __name__ == "__main__":
+    main()
 
